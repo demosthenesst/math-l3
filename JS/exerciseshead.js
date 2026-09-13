d55f8ed347705b6286b4e31a3f0d79d9edd68e73
demosthenesst <127140991+demosthenesst@users.noreply.github.com>
2026-09-13 21:10:50 +0300

    Update exerciseshead.js

--- JS/exerciseshead.js
+++ JS/exerciseshead.js
@@ -14,7 +14,7 @@ const preconnect1 = document.createElement("link");
 preconnect1.rel = "preconnect";
 preconnect1.href = "https://fonts.googleapis.com";
 document.head.appendChild(preconnect1);
-
+//
 const preconnect2 = document.createElement("link");
 preconnect2.rel = "preconnect";
 preconnect2.href = "https://fonts.gstatic.com";
@@ -55,7 +55,6 @@ document.head.appendChild(localCSS);
 // -----------------------------
 // CSS page colors
 // -----------------------------
-
 let currentFile = window.location.pathname.split("/").pop();
 let currentPage = currentFile.replace(/\.html$/, "");
 let html = document.documentElement;
